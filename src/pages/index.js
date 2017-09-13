ffd69b6c3f3414ada4592abc6b1dbd2bbcd6b5d1
sbalcombe <stuartbalcombe@gmail.com>
2017-09-12 22:42:26 -0400

    Update meta data using with React Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,24 @@ class BlogIndex extends React.Component {
 
     return (
       <div>
-        <Helmet title={get(this, "props.data.site.siteMetadata.title")} />
+        <Helmet> 
+          <title>{get(this, "props.data.site.siteMetadata.title")}</title>
+          <meta name="description" content="Product Designer" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@stubalcombe" />
+          <meta name="twitter:title" content="Stuart Balcombe" />
+          <meta name="twitter:description" content="Product Designer" />
+          <meta name="twitter:creator" content="@stubalcombe" />
+          <meta name="twitter:image" content="https://www.stuartbalcombe.com/static/profile-pic.9c85a375.jpg" />
+
+          <meta property="og:title" content="Stuart Balcombe" />
+          <meta property="og:type" content="article" />
+          <meta property="og:url" content="https://www.stuartbalcombe.com/" />
+          <meta property="og:image" content="https://www.stuartbalcombe.com/static/profile-pic.9c85a375.jpg" />
+          <meta property="og:description" content="Product Designer" /> 
+          <meta property="og:site_name" content="Stuart Balcombe" />
+        </Helmet>
         <About />
         {/*
         <BlogIntro />
